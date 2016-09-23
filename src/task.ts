14a9d12c515b0574ce66dacb734c0a0bfe504a45
goenning <me@goenning.net>
2016-09-23 18:59:40 -0300

    fixing some wierd naming problems

---
+++ src/task.ts
@@ -0,0 +1,61 @@
+import { v4 } from "node-uuid";
+
+export interface TaskOptions {
+  priority?: TaskPriority;
+  startAt?: Date | undefined;
+  payload?: any;
+}
+
+export enum TaskPriority {
+    VeryHigh = 1,
+    High = 2,
+    Normal = 3,
+    Low = 4,
+    VeryLow = 5
+}
+
+export class Task {
+  public name: string;
+  public priority: TaskPriority;
+  public id: string;
+  public startAt: Date | undefined;
+  public payload: any;
+
+  constructor(name: string, options?: TaskOptions | (() => TaskOptions)) {
+
+    this.id = v4();
+    this.name = name;
+    let opt: TaskOptions = (typeof options === "function" ? options() : options) || { };
+
+    this.priority = opt.priority ? opt.priority : TaskPriority.Normal;
+    this.startAt = opt.startAt ? opt.startAt : undefined;
+    this.payload = opt.payload ? opt.payload : undefined;
+  }
+
+  getScore(): number {
+    if (this.startAt)
+      return this.startAt.getTime() + this.priority.valueOf();
+    return this.priority.valueOf();
+  }
+
+  toJson(): string {
+    return JSON.stringify({
+      id: this.id,
+      name: this.name,
+      priority: this.priority,
+      startAt: this.startAt,
+      payload: this.payload
+    });
+  }
+
+  static fromJson(input: string): Task {
+    const json = JSON.parse(input);
+    const task = new Task(json.name, {
+      priority: json.priority,
+      startAt: json.startAt ? new Date(json.startAt) : undefined,
+      payload: json.payload
+    });
+    task.id = json.id;
+    return task;
+  }
+}
---
+++ src/yago.ts
@@ -0,0 +1,73 @@
+import { TaskRunner, ITaskRunnerClass, ExecutionContext, ExecutionResult, ExecutionResultOutcome } from "./task_runner";
+import { TaskQueue } from "./task_queue";
+import { Task, TaskOptions } from "./task";
+import { InProcessTaskQueue } from "./inprocess_task_queue";
+import { EventEmitter } from "events";
+import { CronJob } from "cron";
+
+export interface YagoOptions {
+  queue?: TaskQueue;
+  interval?: number;
+  output?: NodeJS.WritableStream;
+}
+
+export class Yago extends EventEmitter {
+  private queue: TaskQueue;
+  private timer: NodeJS.Timer;
+  private output: NodeJS.WritableStream;
+  private runners: { [key: string]: ITaskRunnerClass };
+  private interval: number;
+
+  constructor(options?: YagoOptions) {
+    super();
+    this.runners = {};
+    this.queue = options && options.queue ? options.queue : new InProcessTaskQueue();
+    this.output = options && options.output ? options.output : process.stdout;
+    this.interval = options && options.interval ? options.interval : 1000;
+  }
+
+  register(runner: ITaskRunnerClass): void {
+    if (runner.taskName) {
+      this.runners[runner.taskName] = runner;
+    }
+  }
+
+  schedule(cron: string, taskName: string, options?: TaskOptions): void {
+    new CronJob(cron, () => {
+      this.enqueue(taskName, options);
+    }, undefined, true);
+  }
+
+  enqueue(taskName: string, options?: TaskOptions): Promise<Task> {
+    const task = new Task(taskName, options);
+    this.emit("enqueue", task);
+    return this.queue.enqueue(task);
+  }
+
+  start(): void {
+    this.timer = setInterval(() => {
+      this.processQueue();
+    }, 5);
+  }
+
+  stop(): void {
+    clearInterval(this.timer);
+  }
+
+  private async processQueue(): Promise<ExecutionResult | undefined> {
+    const task = await this.queue.dequeue();
+    if (task) {
+      if (this.runners[task.name]) {
+        const ctx = new ExecutionContext(task, this.output);
+        const runner = new this.runners[task.name]();
+
+        this.emit("process", task);
+        const result = await runner.execute(ctx);
+        this.emit("processed", task, result);
+        return result;
+      } else {
+        this.emit("ignored", task);
+      }
+    }
+  }
+}
