2a27631b67e3dc6c0568543e3715f8e5b3f77802
goenning <me@goenning.net>
2016-09-26 18:27:08 -0300

    clean up samples

--- sample/enqueue.ts
+++
@@ -1,6 +0,0 @@
-import { Task, RedisTaskQueue } from "../src";
-
-const queue = new RedisTaskQueue("redis://localhost:6060");
-queue.enqueue(new Task("greet", () => {
-  return { payload: "Yagoooo" };
-}));
