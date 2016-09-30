4f63303f6eb3430a7dd911a3ffbe37a36f939929
goenning <me@goenning.net>
2016-09-29 22:56:34 -0300

    form bundle to cdn

--- src/public/components/App.tsx
+++ src/public/components/App.tsx
@@ -14,8 +14,8 @@ export class App extends React.Component<HelloProps, {}> {
               <h1>Hello from {this.props.framework}!</h1>
               <select className="ui dropdown">
                 <option value="">Gender</option>
-                <option value="1">Male</option>
-                <option value="0">Female</option>
+                <option value="M">Male</option>
+                <option value="F">Female</option>
               </select>
           </div>;
   }
--- src/public/index.tsx
+++ src/public/index.tsx
@@ -7,8 +7,6 @@ import * as $ from "jquery";
 (window as any).jQuery = (window as any).$  = $;
 
 require("./index.html");
-require("./assets/semantic.min.css");
-require("./assets/semantic.min.js");
 
 ReactDOM.render(
     <App framework="TypeScript" />,
